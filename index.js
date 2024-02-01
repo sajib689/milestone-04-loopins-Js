3ed05bf0f22d837c69ab4087cdc7daf2f82a2084
sajib689 <jibonraja11@gmail.com>
2024-02-01 10:56:56 +0600

    index

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const friends = ['Elon', 'Bill', 'Mark', 'Waren']
 for (const friend of friends) {
     console.log(friend)
 }
+
+for (let i = 0; i < friends.length; i++) {
+    console.log(friends[i])
+    console.log([i])
+}
