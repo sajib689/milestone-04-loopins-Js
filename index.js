42deaba767d10413d58c3d44ce46f3ca1bf29716
sajib689 <jibonraja11@gmail.com>
2024-02-01 11:04:40 +0600

    for loop some new tecniq

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ for (let i = 0; i < friends.length; i++) {
     console.log(friends[i])
     console.log([i])
 }
+const numbers = [3, 2, 1, 0, 44, 1]
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i])
+}
+let i = 0
+while (i > friends.length) {
+    console.log(friends[i])
+}
